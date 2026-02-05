9a7364562abff19a4347a5b5062aeb4bd80d2c0b
uday-venkatesha <udayvenkatesh2015@gmail.com>
2026-02-04 18:55:09 -0600

    Refactor book model and update endpoint response types

--- project.py
+++ project.py
@@ -6,17 +6,29 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class BookCreate(BaseModel):
+    id: int
     title: str
     author: str
     published_date: str
 
-class Book(BookCreate):
+class BookRead(BaseModel):
     id: int
+    title: str
+    author: str
+    published_date: str
 
-@app.post("/books/", response_model=Book)
+    class Config:
+        orm_mode = True
+
+@app.post("/books", response_model=BookRead)
 def create_book(book: BookCreate, db: Session = Depends(get_db)):
     db_book = model.Book(**book.dict())
     db.add(db_book)
     db.commit()
     db.refresh(db_book)
     return db_book
+
+@app.get("/books", response_model=list[BookRead])
+def get_books(db: Session = Depends(get_db)):
+    books = db.query(model.Book).all()
+    return books
